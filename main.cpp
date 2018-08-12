e461f883ff3746f4b3f088834160fade1af1d6a5
KuntimaKiala <kuntima.kiala@gmail.com>
2018-08-12 16:19:58 +0200

    Firsr commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
+/*
+Author : Kuntima
+Description : Shader
+Date : 12/08/2018
+Usage :  g++ -std=c++11 main.cpp Display.cpp GLSLProgram.cpp  Sprite.cpp -o run -lSDL2 -lGL -lGLEW
+		./run
+Note : A lot can be changed, this is nothing more than a prototype that I created to train myself, my goal is to do  3D Game. 
+*/
+
 #include "Display.h"
 #include <iostream>
 
-///Usage : g++ -std=c++11 main.cpp Display.cpp GLSLProgram.cpp  Sprite.cpp -o run -lSDL2 -lGL -lGLEW
+
+
 int main(int argc, char* argv[]){
 
     Display display;
